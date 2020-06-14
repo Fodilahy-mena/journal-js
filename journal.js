fdb9561998f001899bb539ce99ed538bcb33f2d4
JEROME Emilien <jerome.emi@onja.org>
2020-06-14 15:19:50 +0300

    convert the object

--- journal.js
+++ journal.js
@@ -36,36 +36,65 @@ function chooseFromMenu() {
 }
 // work with it
 
-let listArray = [
-    "Title: WHO waiting for evidence on Madagascar's herbal coronavirus cure" +'\n\n' +
+// let listArray = [
+//     "Title: WHO waiting for evidence on Madagascar's herbal coronavirus cure" +'\n\n' +
 
-    "Content: The World Health Organization has yet to receive any data or information about the effectiveness of a herbal cure for Covid-19 promoted by Madagascar's government, the UN health body has said. President Andry Rajoelina has been pushing products containing extracts of the artemisia plant as a treatment for the virus."
-    ,
+//     "Content: The World Health Organization has yet to receive any data or information about the effectiveness of a herbal cure for Covid-19 promoted by Madagascar's government, the UN health body has said. President Andry Rajoelina has been pushing products containing extracts of the artemisia plant as a treatment for the virus."
+//     ,
 
-    "Title: George Floyd death: 'Stop the pain', brother tells US Congress"
-    +'\n\n' +
-    "Content: The brother of the African American man whose death in police custody has sparked global protests, has urged the US Congress to pass reforms on police brutality and 'stop the pain'."
-,
-    "Title: The Forgotten Story of How 13 Black Men Broke the Navy’s Toughest Color Barrier" +'\n\n' +
-    "Content: Sam Barnes racked his brain one chilly morning in January 1944, wondering what he might have done wrong. Barnes, a popular African American petty officer working at Great Lakes Naval Training Station in Illinois, had been in the Navy for 15 months and had never been disciplined. Why, he wondered, was he being ordered to the white side of the segregated station, a command usually reserved for sailors who were in trouble?"
-];
+//     "Title: George Floyd death: 'Stop the pain', brother tells US Congress"
+//     +'\n\n' +
+//     "Content: The brother of the African American man whose death in police custody has sparked global protests, has urged the US Congress to pass reforms on police brutality and 'stop the pain'."
+// ,
+//     "Title: The Forgotten Story of How 13 Black Men Broke the Navy’s Toughest Color Barrier" +'\n\n' +
+//     "Content: Sam Barnes racked his brain one chilly morning in January 1944, wondering what he might have done wrong. Barnes, a popular African American petty officer working at Great Lakes Naval Training Station in Illinois, had been in the Navy for 15 months and had never been disciplined. Why, he wondered, was he being ordered to the white side of the segregated station, a command usually reserved for sailors who were in trouble?"
+// ];
 
-const list = () => {
- 	let randomIndex = Math.floor(Math.random() * listArray.length);
- 	let string = listArray[randomIndex];
- 	const myLists = `"${string}"`;
- 	return myLists;
- };
+// const list = () => {
+//  	let randomIndex = Math.floor(Math.random() * listArray.length);
+//  	let string = listArray[randomIndex];
+//  	const myLists = `"${string}"`;
+//  	return myLists;
+//  };
+
+// With object
+let journalLists = {
+    firstList: {
+        Title: "WHO waiting for evidence on Madagascar's herbal coronavirus cure.",
+        Content: "The World Health Organization has yet to receive any data or information about the effectiveness of a herbal cure for Covid-19 promoted by Madagascar's government, the UN health body has said. President Andry Rajoelina has been pushing products containing extracts of the artemisia plant as a treatment for the virus."
+    },
+    secondList: {
+        Title: "George Floyd death: 'Stop the pain', brother tells US Congress.",
+        Content: "The brother of the African American man whose death in police custody has sparked global protests, has urged the US Congress to pass reforms on police brutality and 'stop the pain'."
+    },
+    thirdList: {
+        Title: "The Forgotten Story of How 13 Black Men Broke the Navy’s Toughest Color Barrier",
+        Content: "Sam Barnes racked his brain one chilly morning in January 1944, wondering what he might have done wrong. Barnes, a popular African American petty officer working at Great Lakes Naval Training Station in Illinois, had been in the Navy for 15 months and had never been disciplined. Why, he wondered, was he being ordered to the white side of the segregated station, a command usually reserved for sailors who were in trouble?"
+    }
+};
+
+//console.log(journalLists.firstList.Title + '\n\n' + journalLists.firstList.Content);
+// const list = () => {
+//  	let randomIndex = Math.floor(Math.random() * listArray.length);
+//  	let string = listArray[randomIndex];
+//  	const myLists = `"${string}"`;
+//  	return myLists;
+//  };
 
 // This is ok!
 
  let menu = Number(prompt(chooseFromMenu()));
-
+ let entry = "";
   if (menu === 1) {
-      for (let i = 0; i < 3; i++) {
-          alert(list());
+      for (let i = 0; i < 1; i++) {
+        alert(journalLists.firstList.Title + '\n\n' + journalLists.firstList.Content);
+        alert(journalLists.secondList.Title + '\n\n' + journalLists.secondList.Content);
+        alert(journalLists.thirdList.Title + '\n\n' + journalLists.thirdList.Content);
       }
+  } else if (menu === 2) {
+        yourEntryTitle = prompt("Enter your jornal title:");
+        yourEntryContent = prompt("Enter your jornal content:");
   } else {
           console.error('Only enter a number between 1 and 5:');
      }
-
+     menu = Number(prompt(chooseFromMenu()));
